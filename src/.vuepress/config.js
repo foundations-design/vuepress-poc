9c5e9c1872d454aa6bc1fd5c99ab22b93e743b5e
earlman <earl.mangulabnan@gmail.com>
2022-03-25 13:08:15 -0500

    update config

---
+++ src/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+   title: "VuePress POC",
+   description: "Just playing around",
+   port: 3000,
+   head: [["script", { src: "https://js.chargebee.com/v2/chargebee.js" }]],
+};
