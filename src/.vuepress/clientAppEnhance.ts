dc8f623180a74de2cfaa3ebb7b691f093a764e0f
earlman <earl.mangulabnan@gmail.com>
2022-03-25 16:38:30 -0500

    add routeGuard

--- src/.vuepress/clientAppEnhance.ts
+++ src/.vuepress/clientAppEnhance.ts
@@ -1,9 +1,23 @@
 import { defineClientAppEnhance } from '@vuepress/client'
-import { createAuth0 } from '@auth0/auth0-vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { createAuth0, authGuard } from '@auth0/auth0-vue'
+
 
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
 
+   // IDK if this is required for authGuards, but it's included in the Auth0 docs
+   router.options.history = createWebHashHistory()
+
+   // There's probably a cleaner way to do this
+   router.beforeResolve(to => {
+      // If I can access the frontmatter here, I can automatically block routes by setting the a frontmatter field (ex. requiresAuth:true)
+      if (to.meta.title === "Gated Example Page") {
+         console.log(to.meta.title)
+         return authGuard(to)
+      }
+   })
+   
    app.use(
       createAuth0({
          domain: 'moonriselabs.us.auth0.com',
@@ -12,4 +26,5 @@ export default defineClientAppEnhance(({ app, router, siteData }) => {
          audience: 'https://api.moonriselabs.com'
       })
    );
+
 })
