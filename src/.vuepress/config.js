cd3b443ea44e04aacb0e9640f6f20c8087320854
earlman <earl.mangulabnan@gmail.com>
2022-04-25 13:03:53 -0500

    add crisp integration

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,7 +6,11 @@ module.exports = {
    description: "Just playing around",
    port: 3000,
    theme: path.resolve(__dirname, './theme'),
-   head: [["script", { src: "https://js.chargebee.com/v2/chargebee.js" }]],
+   head: [["script", { src: "https://js.chargebee.com/v2/chargebee.js" }],
+   ['script', {}, `
+      window.$crisp=[];window.CRISP_WEBSITE_ID="d1a80d59-2880-4eb6-b1b4-7a7e5605e1aa";(function(){let d=document;let s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+    `]
+],
    plugins: [
       {
          define: {
--- src/index.md
+++ src/index.md
@@ -4,8 +4,3 @@
    <Auth />
    <Profile />
 </ClientOnly>
-
-<!-- Add Crisp Support Features -->
-<script type="text/javascript">
-   window.$crisp=[];window.CRISP_WEBSITE_ID="d1a80d59-2880-4eb6-b1b4-7a7e5605e1aa";(function(){let d=document;let s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-</script>
