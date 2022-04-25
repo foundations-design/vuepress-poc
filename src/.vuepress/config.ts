584f22b37a63c6eef42d480fc2ad791895c2ae1e
earlman <earl.mangulabnan@gmail.com>
2022-04-25 16:30:42 -0500

    add favicon and logo

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,12 +9,19 @@ export default defineUserConfig(
       description: "Just playing around",
       port: 3000,
       theme: localTheme({
+
+         // THEME SETTINGS 
+         logo: '/logo.png',
          sidebar: ["/gated-example-message.md", "/gated-example-page.md", "payments.md", "sandbox-loader.md", "custom-markdown-text.md", "mux-video.md"],
+
+   
        }),
-      head: [["script", { src: "https://js.chargebee.com/v2/chargebee.js" }],
-      ['script', {}, `
-         window.$crisp=[];window.CRISP_WEBSITE_ID="d1a80d59-2880-4eb6-b1b4-7a7e5605e1aa";(function(){let d=document;let s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-       `]
+      head: [
+         ['link', { rel: 'icon', href: '/favicon.png' }],
+         ["script", { src: "https://js.chargebee.com/v2/chargebee.js" }],
+         ['script', {}, `
+            window.$crisp=[];window.CRISP_WEBSITE_ID="d1a80d59-2880-4eb6-b1b4-7a7e5605e1aa";(function(){let d=document;let s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+         `]
    ],
       plugins: [
          {
